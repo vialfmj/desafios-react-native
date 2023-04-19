49689269b3e96b704053c726e2f12944cd1b9785
vialfmj <vialfmj@gmail.com>
2023-04-19 14:05:06 -0300

    new expo-project created

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { Text, View } from 'react-native';
+
+import { styles } from './src/styles/styles'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text1}>Desafio Clase2</Text>
+      <Text style={styles.text2}>Hola, Coder!</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ src/styles/styles.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#0000FF',
+      alignItems: 'center',
+      justifyContent: 'space-around',
+      marginTop: 50
+    },
+    text1: {
+      color: '#FFFFFF',
+      fontSize: 30
+    },
+    text2: {
+      color: '#FFFFFF',
+      fontSize: 30
+    }
+  });
